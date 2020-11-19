105e8871786d3de83ed7c9291451a06fc19563b4
user320 <radoslav.mitev@gmx.at>
2020-11-19 18:44:47 +0100

    Apache POI login test implemented

--- src/test/java/dataDrivenTesting/WillhabenLoginTest.java
+++ src/test/java/dataDrivenTesting/WillhabenLoginTest.java
@@ -1,10 +1,27 @@
 package dataDrivenTesting;
 
+/*
+ * Automated Data-Driven Testing implementation of Appium for Android native apps
+ *
+ * Test object – the logon process on the app "Willhaben"
+ *
+ * Test features – three of more possible error alerts:
+ *      "Die angegebene E-Mail-Adresse bzw. das Passwort konnten nicht erkannt werden."
+ *      "E-Mail-Adresse"
+ *      "Passwort"
+ *
+ * This class contains TestNG (negative) tests performing 2 login attempts implementing Apache POI (Excel) and 2 using JDBC (MySQL),
+ * all with incorrect credentials and verifies the expected 3 error alerts.
+ */
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,6 +31,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import pageFactory.WillhabenHomepage;
 import pageFactory.WillhabenLoginPage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.concurrent.TimeUnit;
@@ -29,6 +48,7 @@ public class WillhabenLoginTest {
     private Statement statement;
     private ResultSet resultSet;
     private WebDriverWait wait;
+    DesiredCapabilities capabilities;
 
     /**
      * Verifying that one ot the 3 expected error alerts is displayed:
@@ -52,28 +72,49 @@ public class WillhabenLoginTest {
 
     /**
      * testSetup() performs:
-     * setting up the capabilities,
-     * establishing a connection to the appium server,
-     * page objects initialization,
-     *
+     * setting up the capabilities and
+     * importing data from an excel file.
      */
 
     @BeforeTest
     public void testSetup() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+        this.capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"NEO");
-        capabilities.setCapability(MobileCapabilityType.UDID,"5200a272b26823f7");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"6.0.1");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "at.willhaben");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"at.willhaben.MainActivity");
+        this.capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"NEO");
+        this.capabilities.setCapability(MobileCapabilityType.UDID,"5200a272b26823f7");
+        this.capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        this.capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"6.0.1");
+        this.capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "at.willhaben");
+        this.capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"at.willhaben.MainActivity");
 
+        //importing data from an excel file
+        try {
+//            File src = new File(".\\src\\main\\testdataAndDriver\\Testdata_Excel.xlsx");
+            File src = new File(".\\src\\main\\java\\testdataAndDriver\\Testdata_Excel.xlsx");
+            OPCPackage pkg = OPCPackage.open(src);
+            XSSFWorkbook workbook = new XSSFWorkbook(pkg);
+            this.sheet = workbook.getSheetAt(0);
+            pkg.close();
+        } catch (IOException | InvalidFormatException e) {
+            System.out.println("Fehler: " + e.getMessage());
+        }
+    }
+
+    /**
+     * testSetup() performs:
+     * establishing a connection to the appium server,
+     * page objects initialization,
+     * a click on the "Einloggen" element.
+     *
+     */
+
+
+    @BeforeMethod
+    public void testSet2() {
         try {
             URL url = new URL("http://127.0.0.1:4723/wd/hub");
-            System.out.println("\nremotedriver NOK\n");/////////////
-            this.driver = new AppiumDriver<>(url, capabilities);
-            System.out.println("\nremotedriver ok\n");/////////
+            this.driver = new AppiumDriver<>(url, this.capabilities);
         } catch(Exception e) {
             System.out.println("Cause is: " + e.getCause());
             System.out.println("Error is: " + e.getMessage());
@@ -87,7 +128,6 @@ public class WillhabenLoginTest {
 
         //"Einloggen" element click on
         this.willhabenHomepage.clickLogin();
-
     }
 
 
@@ -160,6 +200,51 @@ public class WillhabenLoginTest {
                 System.out.println("Fehler: " + e.getMessage());
             }
         }
+    }
+
+    /**
+     * The test method imports and uses test data from an EXCEL file:
+     * one pair of credentials in INVALID e-mail format (without @ and domain) and WITHOUT a password:
+     * [excel1@gmx|   ]
+     *
+     * Test feature (error alert): "Passwort"
+     *
+     */
+
+    @Test
+    public void loginUsingExcelFileData_noPassword() {
+
+        this.willhabenLogin.setInputFieldEMail(this.sheet.getRow(1).getCell(0).getStringCellValue());
+
+        //submit
+        this.willhabenLogin.setloginButtonClick();
+
+        //verifying alert message: "Passwort"
+        this.assertExpectedAlertIsDisplayed(this.willhabenLogin.getInputFieldPassword());
+
+        //this.driver.quit();
+    }
+
+    /**
+     * The test method imports and uses test data from an EXCEL file:
+     * one pair of credentials with password but without an e-mail address:
+     * [        | passwordexcel  ]
+     *
+     * Test feature (error alert): "Passwort"
+     */
+
+    @Test
+    public void loginUsingExcelFileData_noEmailAddress() {
+
+        this.willhabenLogin.setInputFieldPassword(this.sheet.getRow(2).getCell(1).getStringCellValue());
+
+        //submit
+        this.willhabenLogin.setloginButtonClick();
+
+        //verifying alert message: "E-Mail-Adresse""
+        this.assertExpectedAlertIsDisplayed(this.willhabenLogin.getInputFieldEMail());
+
+        //this.driver.quit();
     }
 
     @AfterTest
